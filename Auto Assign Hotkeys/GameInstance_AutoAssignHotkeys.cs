50d1ca016a85d724a7dc5aae59a79ef8ce8d4875
ljars <72370023+ljars@users.noreply.github.com>
2021-10-18 18:37:41 -0700

    Add v1.0 scripts

---
+++ Auto Assign Hotkeys/GameInstance_AutoAssignHotkeys.cs
@@ -0,0 +1,23 @@
+﻿/* Auto Assign Hotkey addon for SURIYUN's MMORPG KIT. */
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public partial class GameInstance
+    {
+        /// <summary>
+        /// To disable this addon, set this value to false on the GameInstance object in your Init scene(s).
+        /// </summary>
+        [Tooltip("When true, pre-unlocked skills will be assigned to hotkeys when creating a new character."),
+         Header("Addon: AutoAssignHotkeys")]
+        public bool autoAssignHotkeys = true;
+        
+        /// <summary>
+        /// This specifies by hotkeyID which hotkeys will be assigned skills. The hotkeys will be assigned in the order
+        /// their IDs appear in this array. These should match the hotkeyID in the uiCharacterHotkeys array of the
+        /// UICharacterHotkeys component (look in your CanvasGameplay prefab).
+        /// </summary>
+        [Tooltip("These are the hotkeys to assign skills to. Each should match a hotkeyID in UICharacterHotkeys.")]
+        public string[] hotkeysToAssign = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+    }
+}
---
+++ Auto Assign Hotkeys/PlayerCharacterDataExtension_AutoAssignHotkeys.cs
@@ -0,0 +1,44 @@
+﻿/* Auto Assign Hotkey addon for SURIYUN's MMORPG KIT. */
+using System.Collections.Generic;
+
+namespace MultiplayerARPG
+{
+    public static partial class PlayerCharacterDataExtension
+    {
+        /// <summary>
+        /// This extension method will be called when setting up a new character. It will assign any skills that are
+        /// level 1 or higher to the hotkey slots (until running out of available slots).
+        /// </summary>
+        [DevExtMethods("SetNewCharacterData")]
+        public static void SetNewCharacterData_AutoAssignHotkeys(IPlayerCharacterData character, string characterName,
+            int dataId, int entityId)
+        {
+            if (GameInstance.Singleton == null || !GameInstance.Singleton.autoAssignHotkeys)
+                return;
+
+            string[] hotkeyIDs = GameInstance.Singleton.hotkeysToAssign;
+            Dictionary<BaseSkill, short> skills = character.GetSkills(true);
+            if (skills == null || hotkeyIDs == null)
+                return;
+
+            int hotkeyIndex = 0;
+            foreach (KeyValuePair<BaseSkill, short> skillData in skills)
+            {
+                if (hotkeyIndex >= hotkeyIDs.Length)
+                    return; // No more hotkey slots left
+
+                BaseSkill skill = skillData.Key;
+                short skillLevel = skillData.Value;
+                if (skillLevel > 0 && skill != null && !skill.IsPassive) // Don't assign unlearned or passive skills
+                {
+                    character.Hotkeys.Add(new CharacterHotkey()
+                    {
+                        hotkeyId = hotkeyIDs[hotkeyIndex++],
+                        relateId = skill.Id,
+                        type = HotkeyType.Skill
+                    });
+                }
+            }
+        }
+    }
+}
